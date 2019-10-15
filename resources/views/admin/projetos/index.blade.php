e8aca621ed58a817ec5b6816edcaf05f3a4121cd
Roger M. Brusamarello <34141109+RogerBrusamarello@users.noreply.github.com>
2019-10-14 21:38:48 -0300

    Correção da imagem

--- resources/views/admin/projetos/index.blade.php
+++ resources/views/admin/projetos/index.blade.php
@@ -16,7 +16,7 @@
         <div class="card">
             <div class="card-body justify-content-center" style="width: 15rem;">
                 <a href="{{ route('admin.tarefas') }}">
-                <img src="https://www.trzcacak.rs/myfile/detail/487-4878854_ferramentas-em-png-tools-clip-art.png" alt="+" width="120px">
+                <img src="https://www.trzcacak.rs/myfile/detail/487-4878854_ferramentas-em-png-tools-clip-art.png" alt="editar" width="120px">
                 </a>
                 <h5 class="card-title">{{ $registro->nomeProjeto }}</h5>
                 <p class="card-text">{{ $registro->solicitante }}</p>
@@ -26,9 +26,9 @@
                         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZC8sDxWkT0-hG7ZTUTWOaAtAsAfrdxkOHuo43sagxdXEsMauQOQ&s" alt="editar" width="20px">
                     </a>
                     &nbsp;
-                    &nbsp;
-                    <a href="">
-                        <img src="https://image.flaticon.com/icons/png/512/70/70388.png" alt="+" width="20px"><br>
+                    
+                    <a  href="{{ route('admin.projetos.deletar',$registro->id) }}">
+                        <img src="https://image.flaticon.com/icons/png/512/70/70388.png" alt="apagar" width="20px"><br>
                     </a>
                 </div>
             </div>
